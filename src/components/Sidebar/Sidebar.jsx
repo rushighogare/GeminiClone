2be604c59c405317c89439d9e5527e97e8a33640
Rushikesh Ghogare <rushighogare5561@gmail.com>
2024-07-07 15:29:59 +0530

    file created

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './Sidebar.css'
+
+const Sidebar=()=>{
+    return (
+        <div className="sidebar">
+            <div className="top">
+        
+            </div>
+            <div className="bottom">
+
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
