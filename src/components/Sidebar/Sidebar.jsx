2effc4b1204c88f0109bd27aff336bc9a8b236ab
Rushikesh Ghogare <rushighogare5561@gmail.com>
2024-07-12 23:38:50 +0530

    Added functionality to save prompt in sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,15 +1,21 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import './Sidebar.css'
 import {assets} from '../../assets/assets'
+import { Context } from "../../context/context";
 const Sidebar=()=>{
 
     const [extended, setExtended]=useState(false);
+    const {onSent, prevPrompts, setRecentPrompt, newChat}=useContext(Context);
+    const loadPrompt=async (prompt)=>{
+        setRecentPrompt(prompt)
+        await onSent(prompt)
+    }
 
     return (
         <div className="sidebar">
             <div className="top">
                 <img onClick={()=>setExtended(prev=>!prev)}className="menu" src={assets.menu_icon} alt="" />
-                <div className="new-chat">
+                <div onClick={()=>newChat()}className="new-chat">
                     <img src={assets.plus_icon} alt="" />
                     {extended? <p>New Chat</p>: null}
                 </div>
@@ -17,10 +23,15 @@ const Sidebar=()=>{
                 {extended?
                     <div className="recent">
                         <p className="recent-title">Recent</p>
-                        <div className="recent-entry">
-                            <img src={assets.message_icon} alt="" />
-                            <p>What is react...</p>
-                        </div>
+                        {prevPrompts.map((item, index)=>{
+                            return (
+                                <div onClick={()=>loadPrompt(item)} className="recent-entry">
+                                    <img src={assets.message_icon} alt="" />
+                                    <p>{item.slice(0, 18)}...</p>
+                                </div>
+                            )
+                        })}
+                        
                     </div> 
                 : null }
                 
